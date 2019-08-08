a1606a530a53a32fc7b40cd716c6c059f002b679
insideoutzombie <david@web3devs.com>
2019-08-08 12:00:17 -0500

    cleaned up code, nothing major, dont freak out

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 
 // ### Exercise 1:
 // Use a for loop to print numbers -3 through 8.
-// for (var i = -3; i < 9; i++){
-//   console.log(i);
-// }
-//
-// // ### Exercise 2:
-// // Use a for loop to print from 0 to 50. Once it gets to 10, print "TEN!!!" instead of the number.
-// for (var x = 0; x < 51; x++){
-//   if (x === 10) {
-//     console.log("TEN!!!");
-//   }
-//   console.log(x);
-// }
+for (var i = -3; i < 9; i++){
+  console.log(i);
+}
+
+// ### Exercise 2:
+// Use a for loop to print from 0 to 50. Once it gets to 10, print "TEN!!!" instead of the number.
+for (var x = 0; x < 51; x++){
+  if (x === 10) {
+    console.log("TEN!!!");
+  }
+  console.log(x);
+}
 
 // ### Exercise 3:
 // Prompt the user to enter a number. Prompt the user to enter another number.
@@ -21,10 +21,11 @@
 var numb1 = parseInt(prompt("Enter a number bruh"));
 var numb2 = parseInt(prompt("Enter another number bruh"));
 
-console.log(numb1)
-
-
 for (var y = numb1; y < numb2; y++){
   console.log(y);
-
 }
+
+
+
+
+// making magic
